1737218f452a61f6be035fef07097edacb2f1cea
Maxim <kavai.pika@gmail.com>
2019-03-05 09:04:32 +0200

    Initial commit

---
+++ desktop/src/ru/geekbarains/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbarains.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbarains.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
